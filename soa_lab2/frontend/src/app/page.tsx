b4ff4d2112d3093adb090177268ad33d84afef4e
Овсянников Роман Дмитриевич <ro.ovsyannikov@tbank.ru>
2024-10-19 00:00:55 +0300

    working version

--- soa_lab2/frontend/src/app/page.tsx
+++ soa_lab2/frontend/src/app/page.tsx
@@ -3,44 +3,14 @@ import { useEffect, useState } from 'react';
 
 import { Flat } from './models/Flat';
 import { Transport } from './models/Transport';
-import { Coordinates } from './models/Coordinates';
 import { Furnish } from './models/Furnish';
 import { View } from './models/View';
-import { House } from './models/House';
 import MoneyBalconyFilter from './filters/MoneyBalconyFilter';
 import './Flat.css';
-import xml2js from 'xml2js';
+
+import { parseFlatsFromXml, parseFlatFromXml } from './services/parsers';
 
 const API_URL = 'https://89.169.129.229';
-
-const parseFlatsFromXml = async (xmlResponse: string): Promise<Flat[]> => {
-  const parsedData = await xml2js.parseStringPromise(xmlResponse, { explicitArray: false });
-
-  // Извлекаем данные о квартирах
-  const flatsData = parsedData.Flats?.Flat || parsedData.Flat; // Проверяем наличие родительского тега
-
-  // Если flatsData не определены, возвращаем пустой массив
-  if (!flatsData) {
-    return [];
-  }
-
-  // Проверяем, является ли flatsData массивом. Если нет, преобразуем в массив.
-  const flatsArray = Array.isArray(flatsData) ? flatsData : [flatsData];
-
-  return flatsArray.map((flatData: Flat) => new Flat(
-    Number(flatData.id),
-    flatData.name,
-    new Coordinates(Number(flatData.coordinates.x), Number(flatData.coordinates.y)),
-    Number(flatData.area),
-    Number(flatData.numberOfRooms),
-    flatData.transport as Transport,
-    new Date(flatData.creationDate),
-    flatData.furnish ? flatData.furnish as Furnish : undefined,
-    flatData.view ? flatData.view as View : undefined,
-    flatData.house ? new House(Number(flatData.house.numberOfFloors), Number(flatData.house.numberOfLifts), flatData.house.name, Number(flatData.house.year)) : undefined
-  ));
-};
-
 
 
 const fetchFlats = async (page: number, sortFields: string, filterFields: string): Promise<Flat[]> => {
@@ -60,33 +30,6 @@ const fetchFlats = async (page: number, sortFields: string, filterFields: string
   return parseFlatsFromXml(xmlResponse);
 };
 
-const parseFlatFromXml = (xmlResponse: string): Flat => {
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(xmlResponse, 'text/xml');
-
-  const flat: Flat = {
-    id: parseInt(xmlDoc.getElementsByTagName('id')[0].textContent || '0'),
-    name: xmlDoc.getElementsByTagName('name')[0].textContent || '',
-    coordinates: {
-      x: parseFloat(xmlDoc.getElementsByTagName('x')[0].textContent || '0'),
-      y: parseFloat(xmlDoc.getElementsByTagName('y')[0].textContent || '0'),
-    },
-    creationDate: new Date(xmlDoc.getElementsByTagName('creationDate')[0].textContent || ''),
-    area: parseFloat(xmlDoc.getElementsByTagName('area')[0].textContent || '0'),
-    numberOfRooms: parseInt(xmlDoc.getElementsByTagName('numberOfRooms')[0].textContent || '0'),
-    furnish: xmlDoc.getElementsByTagName('furnish')[0].textContent as Furnish,
-    view: xmlDoc.getElementsByTagName('view')[0].textContent as View,
-    transport: xmlDoc.getElementsByTagName('transport')[0].textContent as Transport,
-    house: {
-      name: xmlDoc.getElementsByTagName('house')[0].getElementsByTagName('name')[0].textContent || '',
-      year: parseInt(xmlDoc.getElementsByTagName('house')[0].getElementsByTagName('year')[0].textContent || '0'),
-      numberOfFloors: parseInt(xmlDoc.getElementsByTagName('house')[0].getElementsByTagName('numberOfFloors')[0].textContent || '0'),
-      numberOfLifts: parseInt(xmlDoc.getElementsByTagName('house')[0].getElementsByTagName('numberOfLifts')[0].textContent || '0'),
-    },
-  };
-
-  return flat;
-};
 
 
 const FlatsTable: React.FC = () => {
@@ -301,7 +244,7 @@ const FlatsTable: React.FC = () => {
   }
 
   if (flats.length === 0) {
-    return <p>"Элементов нет, но вы держитесь"</p>;
+    return <p>Элементов нет, но вы держитесь</p>;
   }
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -314,8 +257,6 @@ const FlatsTable: React.FC = () => {
       setCurrentPage(currentPage - 1);
     }
   };
-
-
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
